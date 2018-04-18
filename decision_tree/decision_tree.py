84d2006bffdf4507184f551b69ebbeaad1bacca4
Felipe Hargreaves <fhargreaves00@gmail.com>
2018-04-18 20:56:01 -0300

    Add knn classifier.
    Co-authored-by: Clarissa Borges <clarissalimab08@gmail.com>
    Co-authored-by: João Vitor <joaovytor0@gmail.com>
    Co-authored-by: Bruna Pinos <bruna.pinos@gmail.com>
    Co-authored-by: Fabio Teixeira <fabio1079@gmail.com>
    Co-authored-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Co-authored-by: Victor Navarro <victor.matias.navarro@gmail.com>

---
+++ decision_tree/decision_tree.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import operator
+import sys
+import zipfile
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+with zipfile.ZipFile("fonts.zip") as fonts_zip:
+    csv_files = fonts_zip.namelist()
+    x = pd.DataFrame()
+    fonts_list = {}
+    for csv in csv_files:
+        font = fonts_zip.open(csv)
+        size = font.read()
+        fonts_list[font] = sys.getsizeof(size)
+        
+    sorted_list = sorted(fonts_list.items(), key=operator.itemgetter(1), reverse=True)
+    chosen_ones = sorted_list[:5]
+    fonts_list = []
+    
+    for font in chosen_ones:
+        font = fonts_zip.open(font[0].name)
+        df = pd.read_csv(font, index_col=None, header=0)
+        fonts_list.append(df)
+
+    x = pd.concat(fonts_list)
+
+y_aux = x[['font']]
+x.drop("h", axis=1, inplace=True)
+x.drop("w", axis=1, inplace=True)
+x.drop('font', axis=1, inplace=True)
+x.drop('fontVariant', axis=1, inplace=True)
+
+
+#transforma label em inteiro
+y = y_aux.apply(LabelEncoder().fit_transform)
+
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3, random_state=42)
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(x_treino, y_treino)
+predictions = clf.predict(x_teste)
+print(accuracy_score(y_teste, predictions))
--- knn/knn.py
+++ knn/knn.py
@@ -5,7 +5,6 @@ import zipfile
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn import tree
 from sklearn.metrics import accuracy_score
 
 with zipfile.ZipFile("fonts.zip") as fonts_zip:
@@ -23,7 +22,7 @@ with zipfile.ZipFile("fonts.zip") as fonts_zip:
     
     for font in chosen_ones:
         font = fonts_zip.open(font[0].name)
-        df = pd.read_csv(font, index_col=None, header=0)
+        df = pd.read_csv(font, index_col=None, header=0, nrows=5000)
         fonts_list.append(df)
 
     x = pd.concat(fonts_list)
@@ -39,9 +38,11 @@ x.drop('fontVariant', axis=1, inplace=True)
 y = y_aux.apply(LabelEncoder().fit_transform)
 
 
-x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3, random_state=42)
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.5, random_state=42)
 
-clf = tree.DecisionTreeClassifier()
-clf.fit(x_treino, y_treino)
-predictions = clf.predict(x_teste)
-print(accuracy_score(y_teste, predictions))
+for i in range(4, 8):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(x_treino, y_treino.values.ravel())
+    print("Knn with " + str(i) + " neighbors")
+    print(knn.score(x_teste, y_teste))
+
